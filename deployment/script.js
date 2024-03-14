0ffd7ab3d34a3d30ff764422dc71c1d33f3f8d5e
Nicolas-Ortiz <nicortiz1234@gmail.com>
2024-03-14 20:33:16 -0300

    Create mobile navbar

--- deployment/script.js
+++ deployment/script.js
@@ -1,11 +1,17 @@
 const menuBtn = document.querySelector('.header .icon-hamburger');
 const menuCloseBtn = document.querySelector('.header .icon-close');
 const navBar = document.querySelector('.header');
+const navBarMobile = document.querySelector('.header-mobile');
+const imgPhone = document.querySelector('#home-section .img-container .phones-img');
 
 menuBtn.addEventListener('click', () => {
-    navBar.classList.toggle('close');
+    navBar.classList.toggle('open');
+    navBarMobile.classList.toggle('open');
+    imgPhone.classList.toggle('open');
 });
 
 menuCloseBtn.addEventListener('click', () => {
-    navBar.classList.toggle('close');
+    navBar.classList.toggle('open');
+    navBarMobile.classList.toggle('open');
+    imgPhone.classList.toggle('open');
 });
